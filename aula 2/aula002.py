7ebf7da171a2c16d22e8053a26ca83396e3f9c88
varelabeatrizz <biaavarelasilvaa.051602@gmail.com>
2024-12-03 09:54:08 -0300

    Add files via upload

---
+++ aula 2/aula002.py
@@ -0,0 +1,14 @@
+print ("hi, world")
+print ("tudo bem com voces")
+print ("hoje meu dia já começou já começou estressante")
+print ("as pessoas ficam conversas de um lado pra outro ")
+print ("e não querem saber a real história antes de começar a falar as coisas")
+print ("sem sair a raiz da situação")
+nome_usuario = 'Ana Beatriz'
+idade_usuario = 17
+altura_usuario = 1.66
+maior_idade = False
+print ('seja bem - vinda', nome_usuario)
+print ('sua idade é', idade_usuario, 'anos.')
+print ('voce tem' ,altura_usuario,'metros.')
+print ('voce é maior de idade?', maior_idade)
